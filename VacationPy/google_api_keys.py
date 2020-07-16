44205aaa758555611fbe99e19dc4056fecdff90d
Danny De la Torre <danny_delatorre@Dannys-MacBook-Pro.local>
2020-07-16 16:46:21 -0400

    psuh

--- VacationPy/google_api_keys.py
+++
@@ -1,2 +0,0 @@
-# Google API Key
-g_key = "AIzaSyDThikQfLKe_sR-PhQli8lTkKQjqJfFjNI"
--- WeatherPy/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "86e091696d04e4636555f97b1ff31889"
-
-# Google API Key
-g_key = "YOUR KEY HERE!"
